b30197612ad1b48624e401efde8a8f7b8ef04289
happycooldude <pieterhuisman2000@gmail.com>
2019-09-11 10:21:41 +0200

    De pizzacalculator gemaakt

---
+++ Pizzacalculator.js
@@ -0,0 +1,23 @@
+//© Pieter Huisman 99047256
+//Applicatie en media ontwikkelaar
+
+var formaatklein = prompt("hoeveel kleine pizza's");
+var formaatmedium = prompt("hoeveel medium pizza's");
+var formaatgroot = prompt("hoeveel grootte pizza's");
+document.write(formaatklein, " kleine pizza's" + "<br>");
+document.write(formaatmedium, " medium pizza's" + "<br>");
+document.write(formaatgroot, " grootte pizza's" + "<br>");
+
+document.write("<br>");
+
+const prijsklein = 3;
+const prijsmedium = 5;
+const prijsgroot = 7;
+document.write("prijs klein €", formaatklein * prijsklein + "<br>");
+document.write("prijs medium €", formaatmedium * prijsmedium + "<br>");
+document.write("prijs goot €", formaatgroot * prijsgroot + "<br>");
+
+document.write("<br>");
+
+var totaalprijs = (formaatklein * prijsklein + formaatmedium * prijsmedium + formaatgroot * prijsgroot);
+document.write("totaalprijs €", totaalprijs);
